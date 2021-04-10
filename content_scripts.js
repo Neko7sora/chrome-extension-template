95c89cf35c913fe4f720f3807a4cfee0110d5747
Neko7sora <75793267+Neko7sora@users.noreply.github.com>
2021-04-10 18:02:59 +0900

    fix

--- content_scripts.js
+++ content_scripts.js
@@ -1,3 +1,10 @@
 chrome.runtime.onMessage.addListener(function (msg) {
     $("body").css("background-color", msg.color);
 });
+chrome.runtime.sendMessage({ log: "start" }, function (response) {
+    console.log(response.status); // → startをセットしました。
+});
+console.log = ((logTextAreaArgument) => {
+    let logTextArea = logTextAreaArgument;
+    return text => logTextArea.value+=text+'\n';
+  })(document.getElementsByClassName('log')[0]);
